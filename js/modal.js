ceb4a78653feb4328c4aa08a336a37ce55d63371
ValKa04 <val.kalinovska@gmail.com>
2022-11-14 21:07:16 +0200

    added js for modal cart and fixed header

---
+++ js/modal.js
@@ -0,0 +1,28 @@
+// JS for the fixed header
+
+const{ height: pageHeaderHeight}=document
+.querySelector(".header-border")
+.getBoundingClientRect();
+
+document.body.style.paddingTop = `${pageHeaderHeight}px`;
+
+// JS for the modal window
+
+const refs = {
+  openModalBtn: document.querySelector("[data-open-modal]"),
+  closeModalBtn: document.querySelector("[data-close-modal]"),
+  backdrop: document.querySelector("[data-backdrop]"),
+};
+
+refs.openModalBtn.addEventListener("click", toggleModal)
+refs.closeModalBtn.addEventListener("click", toggleModal);
+
+refs.backdrop.addEventListener("click", logBackdropClick);
+
+function toggleModal() {
+  refs.backdrop.classList.toggle("is-hidden")
+}
+
+function logBackdropClick(){
+  console.log("it's click")
+}
