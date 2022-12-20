99ccd07dd2c03b4ac7d7c48b4a651982d3cd3278
Valeriia <109133290+ValKa04@users.noreply.github.com>
2022-12-20 16:59:02 +0200

    updated sass components

--- js/modal.js
+++ js/modal.js
@@ -6,39 +6,55 @@ const{ height: pageHeaderHeight}=document
 
 document.body.style.paddingTop = `${pageHeaderHeight}px`;
 
-// JS for the modal window
-
-const refs = {
-  openModalBtn: document.querySelector("[data-open-modal]"),
-  closeModalBtn: document.querySelector("[data-close-modal]"),
-  backdrop: document.querySelector("[data-backdrop]"),
-};
-
-refs.openModalBtn.addEventListener("click", toggleModal)
-refs.closeModalBtn.addEventListener("click", toggleModal);
-
-refs.backdrop.addEventListener("click", logBackdropClick);
-
-function toggleModal() {
-  refs.backdrop.classList.toggle("is-hidden")
-}
-
-function logBackdropClick(){
-  console.log("it's click")
-}
-
-// JS FORM
 
 (() => {
-  document
-  .querySelector('.contact-form')
-  .addEventListener('submit', e=> {
-    e.preventDefault();
-    new FormData(e.currentTarget).forEach((value, name) =>
-    console.log(`${name}: ${value}`),
-    );
-    
-// auto reset after submition ( btn "submit")
-    e.currentTarget.reset();
-  });
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
 })();
+
+// // JS for the modal window
+
+// const refs = {
+//   openModalBtn: document.querySelector("[data-open-modal]"),
+//   closeModalBtn: document.querySelector("[data-close-modal]"),
+//   backdrop: document.querySelector("[data-backdrop]"),
+// };
+
+// refs.openModalBtn.addEventListener("click", toggleModal)
+// refs.closeModalBtn.addEventListener("click", toggleModal);
+
+// refs.backdrop.addEventListener("click", logBackdropClick);
+
+// function toggleModal() {
+//   refs.backdrop.classList.toggle("is-hidden")
+// }
+
+// function logBackdropClick(){
+//   console.log("it's click")
+// }
+
+// // JS FORM
+
+// (() => {
+//   document
+//   .querySelector('.contact-form')
+//   .addEventListener('submit', e=> {
+//     e.preventDefault();
+//     new FormData(e.currentTarget).forEach((value, name) =>
+//     console.log(`${name}: ${value}`),
+//     );
+    
+// // auto reset after submition ( btn "submit")
+//     e.currentTarget.reset();
+//   });
+// })();
